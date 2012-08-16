cbba5d9d742654e4cca9cbc024f894931c229d6e
duniyagwan <duniyagwan@gmail.com>
2012-08-16 17:50:47 -0400

    added some stuff

--- budgetcalculator/includes/db.php
+++ budgetcalculator/includes/db.php
@@ -7,3 +7,8 @@ $name = getenv('MYSQL_DB_NAME');
 
 
 $date_source = sprintf('mysql:host%s;dbname=%s' , $host, $name);
+
+//PDO -> php data objects
+$db = new PDO($data_source, $user, $pass);
+//Make the connection to speak in UTF 8 - don't expect anything back
+$db->exec('SET NAMES utf8');
